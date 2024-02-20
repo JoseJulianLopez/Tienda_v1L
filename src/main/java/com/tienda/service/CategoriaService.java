1f223ca97a631a3fc2400b4d05825790512a7d90
JoseJulianLopez <jlopez30608@ufide.ac.cr>
2024-02-19 22:09:13 -0600

    Semana 6
    Agregando cambios de semana 6

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+    
+    //Se obtiene Categoria, a partir del id de una categoria
+    public Categoria getCategoria (Categoria categoria);
+    
+    public void save (Categoria categoria);
+    
+    public void delete (Categoria categoria);    
+
+}
